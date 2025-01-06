f8fc41cea2d7c39f00bb5e0f5a9eb6ced9b7e0bd
sadiaism <ismailmoiz80@gmail.com>
2025-01-06 14:14:41 +0500

    the sanity

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { urlFor } from "@/sanity/lib/image";
 
 interface Iblog{
   name:string,
-  image:any,
+  image:string,
   buttonText:string
 }
 
@@ -14,18 +14,18 @@ export default async function Home() {
   return (
       <div className="grid grid-cols-1 md:grid-cols-3 gap-[12px]">
         {
-          res.map((data) =>{
+          res.map((blog,index) =>{
            
             return(
-             <div className="flex flex-col justify-center items-center gap-[24px]">
-              <h1 className="text-[24px] font-bold text-blue-800">{data.name}</h1>
+             <div key={index} className="flex flex-col justify-center items-center gap-[24px]">
+              <h1 className="text-[24px] font-bold text-blue-800">{blog.name}</h1>
 
-              <img className="rounded-full" src ={urlFor(data.image).url()}
+              <img className="rounded-full" src ={urlFor(blog.image).url()}
               alt ="Image"
               width ={300}
               height ={300}/>
 
-              <h2 className="border-[1px] border-[#000000] w-[150px] h-[40px] rounded-2xl p-[7px] pl-[24px] bg-black text-white hover:bg-slate-500">{data.buttonText}</h2>
+              <h2 className="border-[1px] border-[#000000] w-[150px] h-[40px] rounded-2xl p-[7px] pl-[24px] bg-black text-white hover:bg-slate-500">{blog.buttonText}</h2>
              </div>
             )
           })
