ca79c5bfbbd2c768295aa602ac45ca66377b6375
sadiaism <ismailmoiz80@gmail.com>
2025-02-12 23:37:02 +0500

    the san blogs

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,6 +11,12 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
+export const token = assertValue(
+  "sk54ErLFHCQi7WhtafYvcUNBrkzEBhifhug4Cxec0IJ40PwjZp8Idy1q5l3ljxanc1isZx8BKcQ6K399BSSiSuEVgIQPXz8ragA0JZ7HRx5TSFlA1rexq7DLlIhVrXsCvotg3ulhUqyvVUSrQoWCSMpvef3bE6CNLW14e0sBJUdJRt1CxRnU",
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+  )
+  
+
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage)
